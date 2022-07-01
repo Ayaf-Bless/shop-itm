c7d772d137c140915af5808f75c9e054fd887411
Ayaf-Bless <blessambel1@gmail.com>
2022-07-01 14:52:20 +0300

    add the graphql backend to the frontend and fetch from it

--- framework/shopify/products/get-all-products.ts
+++ framework/shopify/products/get-all-products.ts
@@ -1,17 +1,26 @@
-const fetchAPI = async () => {
-  const url = "https://jsonplaceholder.typicode.com/todos";
+import { getAllProductsQuery } from "../utils/queries/get-all-products";
+
+type FetchParam = {
+  query: string;
+};
+
+const fetchAPI = async ({ query }: FetchParam) => {
+  const url = "http://localhost:4000/graphql";
 
   const res = await fetch(url, {
-    method: "GET",
+    method: "POST",
     headers: {
       "Content-type": "application/json",
     },
+    body: JSON.stringify({
+      query,
+    }),
   });
   const data = await res.json();
   return { data };
 };
 
 export const getAllProducts = async () => {
-  const products = await fetchAPI();
+  const products = await fetchAPI({ query: getAllProductsQuery });
   return products.data;
 };
