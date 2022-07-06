4252417f4ef8ccbfc6ef30bd4b534e6e8c6b9247
Ayaf-Bless <blessambel1@gmail.com>
2022-07-06 21:14:27 +0300

    setting up the markup for ProductCard

--- components/product/productCard/ProductCard.tsx
+++ components/product/productCard/ProductCard.tsx
@@ -1,6 +1,8 @@
 import { Product } from "@common/types/product";
 import React from "react";
 import { ChildrenProps } from "@components/common/type";
+import Link from "next/link";
+import Image from "next/image";
 
 interface Props {
   product: Product;
@@ -9,7 +11,26 @@ interface Props {
 const ProductCard: React.FC<ChildrenProps & Props> = ({ product }) => {
   return (
     <>
-      <div>{product.name}</div>
+      <Link href={`/products/${product.slug}`}>
+        <a>
+          <div>
+            <h3>
+              <span>{product.name}</span>
+            </h3>
+            <span>$14</span>
+          </div>
+          {product.images && (
+            <Image
+              src={"/product-image-placeholder.svg"}
+              alt={product.name ?? "Product image"}
+              height={540}
+              width={540}
+              quality={"58"}
+              layout={"responsive"}
+            />
+          )}
+        </a>
+      </Link>
     </>
   );
 };
