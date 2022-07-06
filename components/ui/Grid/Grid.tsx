7ba6b8d7a7415e977804e27423cb6696f97ec657
Ayaf-Bless <blessambel1@gmail.com>
2022-07-07 02:39:26 +0300

    create different layout style and change them dinamically

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import cn from "classnames";
 import { ChildrenProps } from "@components/common/type";
 import style from "./Grid.module.css";
 
-const Grid: React.FC<ChildrenProps> = ({ children }) => {
-  return <div className={style.root}>{children}</div>;
+interface Props {
+  layout?: "A" | "B";
+}
+
+const Grid: React.FC<ChildrenProps & Props> = ({ children, layout = "A" }) => {
+  const classRoot = cn(style.root, {
+    [style.layoutA]: layout === "A",
+    [style.layoutB]: layout === "B",
+  });
+  return <div className={classRoot}>{children}</div>;
 };
 
 export default Grid;
