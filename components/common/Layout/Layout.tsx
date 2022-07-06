ab1c1028a495a8897dd59fc803a86be6e1d7f553
Ayaf-Bless <blessambel1@gmail.com>
2022-07-06 18:32:57 +0300

    create a granular css to fit the component model

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { AuxProps } from "@components/common/type";
+import style from "./Layout.module.css";
 
 const Layout: React.FC<AuxProps> = ({ children }): JSX.Element => {
-  return <div className={"layout"}>{children}</div>;
+  return (
+    <>
+      <main className={style.root}>{children}</main>;
+    </>
+  );
 };
 
 export default Layout;
