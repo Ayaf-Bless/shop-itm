cfd439edf9fe06499b7296908b3f74a1e4b92757
Ayaf-Bless <blessambel1@gmail.com>
2022-07-21 14:54:29 +0300

    react-ticker bug still pending...

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { ChildrenProps } from "@components/common/type";
 import style from "./Marquee.module.css";
+import Ticker from "react-ticker";
 
 const Marquee: React.FC<ChildrenProps> = ({ children }) => {
   return (
     <div className={style.root}>
+      {/*<Ticker>{() => <div className={style.container}>{children}</div>}</Ticker>*/}
       <div className={style.container}>{children}</div>
+      {/*<Ticker>{() => <p>Hello</p>}</Ticker>*/}
     </div>
   );
 };
